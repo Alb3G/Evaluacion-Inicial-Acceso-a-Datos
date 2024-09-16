85ca16e24af1687eef7819951edd16cc6ca189aa
Alberto <guzmanmorenoalberto1998@gmail.com>
2024-09-16 12:26:25 +0200

    Sout added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,6 +14,7 @@ public class Main {
             System.out.println("Escriba el numero final:");
             end = sc.nextInt();
             ArrayList<Integer> primeNums = searchPrimeNumbers(start, end);
+            System.out.println("Buscando primos...");
             for(int n : primeNums) {
                 System.out.println(n);
             }
